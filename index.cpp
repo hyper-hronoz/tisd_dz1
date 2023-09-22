3575867623fb097dd645cd3263ad9d62eb18fcd3
hyper-hronoz <vlad_moryakov_prog@mail.ru>
2023-09-22 10:53:55 +0300

    sum addition and format correction

--- index.cpp
+++ index.cpp
@@ -1,5 +1,7 @@
 #include "iostream"
 #include "string"
+#include <algorithm>
+#include <bitset>
 #include <cstdint>
 #include <sstream>
 #include <vector>
@@ -14,52 +16,48 @@ struct bfloat {
   uint8_t sign : 1;
 };
 
-// 101 bit
-struct bint {
-  // there must be 30 symbols
-  uint64_t part_l : 64;
-  uint64_t part_h : 36;
-
-  uint8_t sign : 1;
-};
-
-class bint_t {
-public:
-  bint_t(const char *chars) {
-    int counter = 0;
-    bint my_int;
-
-    char to_fixed[30] = {'B'};
-
-    while (*chars) {
-
-      if (counter == 0 && *chars == '-') {
-        my_int.sign |= 0b1;
-        continue;
-      }
-
-      if (counter == 0 && *chars == '+') {
-        my_int.sign &= ~(0b1);
-        continue;
-      }
-
-      if (48 <= *chars && *chars <= 57) {
-        to_fixed[counter] = *chars;
-      } else {
-        cout << "NOT A NUMBER IN bint constructor" << endl;
-      }
-      chars++;
-      counter++;
+string string_multiply(string nums1, string nums2) {
+  int n = nums1.size();
+  int m = nums2.size();
+  string ans(n + m, '0');
+  for (int i = n - 1; i >= 0; i--) {
+    for (int j = m - 1; j >= 0; j--) {
+      int p = (nums1[i] - '0') * (nums2[j] - '0') + (ans[i + j + 1] - '0');
+      ans[i + j + 1] = p % 10 + '0';
+      ans[i + j] += p / 10;
     }
-
-    for (uint8_t i = 0; i < 30; i++) {
-      cout << to_fixed[i] << endl;
-    }
-
-    cout << to_fixed[0] << endl;
-    cout << to_fixed[1] << endl;
   }
-};
+  for (int i = 0; i < m + n; i++) {
+    if (ans[i] != '0')
+      return ans.substr(i);
+  }
+  return "0";
+}
+
+string string_sum(string a, string b) {
+  if (a.size() < b.size())
+    swap(a, b);
+
+  int j = a.size() - 1;
+  for (int i = b.size() - 1; i >= 0; i--, j--)
+    a[j] += (b[i] - '0');
+
+  for (int i = a.size() - 1; i > 0; i--) {
+    if (a[i] > '9') {
+      int d = a[i] - '0';
+      a[i - 1] = ((a[i - 1] - '0') + d / 10) + '0';
+      a[i] = (d % 10) + '0';
+    }
+  }
+  if (a[0] > '9') {
+    string k;
+    k += a[0];
+    a[0] = ((a[0] - '0') % 10) + '0';
+    k[0] = ((k[0] - '0') / 10) + '0';
+    a = k + a;
+  }
+  return a;
+}
 
 string long_div(string number, int divisor) {
   // As result can be very large store it in string
@@ -81,39 +79,114 @@ string long_div(string number, int divisor) {
   return ans;
 }
 
-int main() {
-  stringstream geek;
+bool fullAdder(bool b1, bool b2, bool &carry) {
+  bool sum = (b1 ^ b2) ^ carry;
+  carry = (b1 && b2) || (b1 && carry) || (b2 && carry);
+  return sum;
+}
 
-  string long_string = long_div("999999999999999999999999999999", 1);
-  string prev_long_string;
+bitset<101> bitsetAdd(bitset<101> &x, bitset<101> &y) {
+  bool carry = false;
+  // bitset to store the sum of the two bitsets
+  bitset<101> ans;
+  for (int i = 0; i < 100; i++) {
+    ans[101 - i - 1] = fullAdder(x[101 - i - 1], y[101 - i - 1], carry);
+  }
+  return ans;
+}
 
-  vector<int> result = {};
+class bint_t {
+private:
+  bitset<101> bint;
 
-  while (long_string != "0" && long_string != "1") {
-    char last_char_of_long_string = long_string[long_string.length() - 1];
-    uint8_t remainder = 0;
+public:
+  explicit bint_t() {}
+  bint_t(bitset<101> bits) { this->bint = bits; }
 
-    switch (last_char_of_long_string) {
-    case '1':
-    case '3':
-    case '5':
-    case '7':
-    case '9':
-      remainder = 1;
+  bint_t(string long_string) {
+    if (long_string[0] == '+') {
+      long_string[0] = '0';
+      bint[0] = 0b0;
     }
 
-    long_string = long_div(long_string, 2);
+    if (long_string[0] == '-') {
+      long_string[0] = '0';
+      bint[0] = 0b1;
+    }
 
-    result.push_back(remainder);
+    uint8_t is_first_not_null_met = 0;
+    string new_string;
+    for (uint8_t i = 0; i < long_string.length(); i++) {
+      if (long_string[i] != '0' && !is_first_not_null_met) {
+        is_first_not_null_met = 1;
+      }
+      if (is_first_not_null_met) {
+        new_string.push_back(long_string[i]); 
+      }
+    }
+
+    long_string = new_string;
+
+    uint8_t counter = 0;
+    while (long_string != "0" && long_string != "1") {
+      char last_char_of_long_string = long_string[long_string.length() - 1];
+      uint8_t remainder = 0;
+
+      switch (last_char_of_long_string) {
+      case '1':
+      case '3':
+      case '5':
+      case '7':
+      case '9':
+        remainder = 1;
+      }
+
+      long_string = long_div(long_string, 2);
+      bint[101 - 1 - counter] = (remainder);
+      counter++;
+    }
+
+    if (long_string == "1") {
+      bint[101 - 1 - counter] = (1);
+    }
+
+    print();
   }
 
-  if (long_string == "1") {
-    result.push_back(1);
-  } else {
-    result.push_back(0);
+  bint_t operator+(bint_t &right_bint) {
+    bitset<101> m = bitsetAdd(this->bint, right_bint.bint);
+    return bint_t(m);
   }
 
-  for (auto item : result) {
-    cout << item << endl;
+  void print() {
+    for (int i = 0; i < 101; i++) {
+      cout << bint[i];
+    }
+    cout << endl;
   }
+
+  void print_dec() {
+    string sum = "0";
+    for (uint8_t i = 0; i < 100; i++) {
+      uint8_t binary_number = this->bint[101 - i - 1];
+      string number = to_string(binary_number);
+      for (uint8_t j = 0; j < i; j++) {
+        if (i != 0) {
+          number = string_multiply(number, "2");
+        }
+      }
+      sum = string_sum(number, sum);
+    }
+    cout << sum << endl;
+  }
+};
+
+int main() {
+  bint_t by_n1("-00000000001");
+  by_n1.print();
+  // bint_t by_n2("2");
+
+  // bint_t by_n3 = by_n1 + by_n2;
+  // by_n3.print();
+  // by_n3.print_dec();
 }
